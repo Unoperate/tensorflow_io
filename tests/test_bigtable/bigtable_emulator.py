877f644d1c975bfdca07ff3802f3ab347731c540
Kajetan Boroszko <kajetan@unoperate.com>
2021-12-27 11:20:21 +0100

    fixed cbt emulator path

--- tests/test_bigtable/bigtable_emulator.py
+++ tests/test_bigtable/bigtable_emulator.py
@@ -28,12 +28,14 @@ from threading import Thread
 from typing import List
 
 CBT_EMULATOR_SEARCH_PATHS = [
+    "google-cloud-sdk/platform/bigtable-emulator/cbtemulator",
     "/usr/lib/google-cloud-sdk/platform/bigtable-emulator/cbtemulator",
     "/usr/local/google-cloud-sdk/platform/bigtable-emulator/cbtemulator",
     "cbtemulator",
 ]
 
 CBT_CLI_SEARCH_PATHS = [
+    "google-cloud-sdk/bin/gcloud",
     "/usr/local/google-cloud-sdk/bin/cbt",
     "/usr/bin/cbt",
     "cbt",
